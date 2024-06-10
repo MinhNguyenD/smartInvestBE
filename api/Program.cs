e6e3dd5099c5866a2295bd11f8e1c21ac3afd2f9
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-06-10 00:19:47 -0300

    Scaffolding project

---
+++ api/Program.cs
@@ -0,0 +1,19 @@
+// configure services and DI
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// control http request pipeline
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
