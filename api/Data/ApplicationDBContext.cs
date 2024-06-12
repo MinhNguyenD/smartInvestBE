f41f3b91561e2a87de8fbeb6a39c3fb9097ce9da
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-06-12 01:00:20 -0300

    Finished setting up DB, created controller with CRUD

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -8,8 +8,11 @@ namespace api.Data
 {
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions){
-
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
         }
+
+        public DbSet<Stock> Stocks {get; set;}
     }
 }
