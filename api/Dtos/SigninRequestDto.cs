bef103927ec110642cad383956aa0883791f6117
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-02 16:07:42 -0300

    Implemented Login, finished up auth

---
+++ api/Dtos/SigninRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class SigninRequestDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
