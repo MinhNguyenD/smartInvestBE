eefa06df9e799aaf5d5f5a4b2995210e92969aa5
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-23 18:07:27 -0300

    Separated to 2 mciroservices

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -12,8 +12,6 @@ namespace api.Data
 
         }
 
-        public DbSet<Stock> Stocks { get; set; }
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
