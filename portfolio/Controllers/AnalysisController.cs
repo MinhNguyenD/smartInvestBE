4dcb297cdcceaa5ae7a33e8be22fc57f814cee7b
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-26 18:18:00 -0300

    Refactor email service

--- portfolio/Controllers/AnalysisController.cs
+++ portfolio/Controllers/AnalysisController.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -17,10 +20,14 @@ namespace portfolio.Controllers
     public class AnalysisController : ControllerBase
     {
         private readonly IAnalysisService _analyzeService;
+        private readonly IAmazonSimpleNotificationService _snsClient;
+        private string _topic = "SmartInvest-";
 
-        public AnalysisController(IAnalysisService analyzeService)
+
+        public AnalysisController(IAnalysisService analyzeService, IAmazonSimpleNotificationService snsClient)
         {
             _analyzeService = analyzeService;
+            _snsClient = snsClient;
         }
 
 
@@ -89,6 +96,53 @@ namespace portfolio.Controllers
             catch(Exception e){
                 return StatusCode(500, e.Message);
             }
+        }
+
+        [HttpPost("{id:int}/email")]
+        public async Task<IActionResult> SendEmail(int id){
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var username = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            var analysis = await _analyzeService.GetAnalysisByIdAsync(id);
+            if(analysis == null){
+                return NotFound("Analysis not found");
+            }
+            var topicArnExists = await _snsClient.FindTopicAsync(_topic);
+            string topicArn = "";
+            if (topicArnExists == null)
+            {
+                var createTopicResponse = await _snsClient.CreateTopicAsync(_topic);
+                topicArn = createTopicResponse.TopicArn;
+                SubscribeRequest subscribeRequest = new SubscribeRequest()
+                {
+                    TopicArn = topicArn,
+                    ReturnSubscriptionArn = true,
+                    Protocol = "email",
+                    Endpoint = "MinhNguyen@dal.ca",
+                };
+
+                var response = await _snsClient.SubscribeAsync(subscribeRequest);
+            }
+            else
+            {
+                topicArn = topicArnExists.TopicArn;
+            }
+
+            var message = analysis.Content;
+            var doc = new HtmlDocument();
+            doc.LoadHtml(message);
+            message = doc.DocumentNode.InnerText;
+
+            var subject = $"{analysis.StockSymbol}'s Financial Analysis";
+            //create and publish a new message to the sns topic arn
+            var publishRequest = new PublishRequest()
+            {
+                TopicArn = topicArn,
+                Message = message,
+                Subject = subject
+            };
+
+            await _snsClient.PublishAsync(publishRequest);
+            return Ok();
         }
 
         [HttpDelete("{id:int}")]
