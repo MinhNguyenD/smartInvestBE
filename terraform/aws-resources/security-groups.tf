7ca3067524be0ce7aad0e59cf5a4124647371554
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-08-05 22:48:32 -0300

    Added security groups for group node

--- terraform/aws-resources/security-groups.tf
+++ terraform/aws-resources/security-groups.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "smartinvest-ec2-fe-sg" {
   name        = "smartinvest-ec2-fe"
-  description = "Allow HTTPs traffic to EC2 front end"
+  description = "Allow HTTP, HTTPs, SSH traffic to EC2 front end"
   vpc_id      = aws_vpc.main.id
   ingress {
     from_port        = 80
@@ -30,6 +30,41 @@ resource "aws_security_group" "smartinvest-ec2-fe-sg" {
     Name = "smartinvest-ec2-fe"
   }
 }
+
+resource "aws_security_group" "smartinvest-eks-node-sg" {
+  name        = "smartinvest-eks-node"
+  description = "Allow HTTP, HTTPs, SSH traffic to EKS node from internal vpc"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["10.0.0.0/16"] # all main vpc traffic
+  }
+  tags = {
+    Name = "smartinvest-eks-node"
+  }
+}
+
+
 
 resource "aws_security_group" "smartinvest-rds-sg" {
   name        = "smartinvest-rds-sg"
