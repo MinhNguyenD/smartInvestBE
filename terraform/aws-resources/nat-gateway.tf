b8d7b9ad358c5495413d03c05535541d2d20168b
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-08-06 01:18:09 -0300

    Changed eip

--- terraform/aws-resources/nat-gateway.tf
+++ terraform/aws-resources/nat-gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "smartinvest-natgw" {
   # elastic ip 
-  allocation_id = aws_eip.natgw-ip.id
+  allocation_id = aws_eip.smartinvest-natgw-eip.id
   subnet_id     = aws_subnet.smartinvest-public-subnet-1a.id
 
   tags = {
@@ -8,5 +8,5 @@ resource "aws_nat_gateway" "smartinvest-natgw" {
   }
 
   # To ensure proper ordering, add an explicit dependency on the Internet Gateway for the VPC.
-  depends_on = [aws_eip.natgw-ip, aws_internet_gateway.smartinvest-ig]
+  depends_on = [aws_eip.smartinvest-natgw-eip, aws_internet_gateway.smartinvest-ig]
 }
